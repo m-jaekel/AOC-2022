e34281ea75bc46d1e2e9680664a500b2f2dfa57e
m-jaekel <m-jaekel>
2022-12-01 20:17:16 +0100

    Day01: part 1
    Signed-off-by: m-jaekel <m-jaekel>

--- src/day01/Day01_Part1.kt
+++ src/day01/Day01_Part1.kt
@@ -1,10 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var calories = listOf<Int>()
+
+        var startIndex = 0
+        input.forEachIndexed { index, s ->
+            if (s.isBlank()) {
+                calories = calories + listOf(input.subList(startIndex, index).sumOf { it.toInt() })
+                startIndex = index + 1
+            }
+        }
+        return calories.max()
     }
 
     val testInput = readInput("day01/test_input")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("day01/input")
     println(part1(input))
