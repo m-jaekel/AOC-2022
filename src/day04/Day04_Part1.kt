e144bd2721ef67cc72a8d3af0f7d802bc8297e89
m-jaekel <m-jaekel>
2022-12-04 12:35:59 +0100

    Day04: part1
    Signed-off-by: m-jaekel <m-jaekel>

--- src/day04/Day04_Part1.kt
+++ src/day04/Day04_Part1.kt
@@ -1,11 +1,26 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val elfPairs = input.map { pairs ->
+            pairs.split(",").let { elves ->
+                elves.map { elf ->
+                    elf.split("-").map { ids -> ids.toInt() }.let { (it[0]..it[1]).toSet() }
+                }
+            }
+        }
+
+        var fullContainCount = 0
+        elfPairs.forEach {
+            if (it[0].containsAll(it[1]) || it[1].containsAll(it[0])) {
+                fullContainCount++
+            }
+        }
+        return fullContainCount
     }
 
     val testInput = readInput("day04/test_input")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 2)
 
     val input = readInput("day04/input")
     println(part1(input))
