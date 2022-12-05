42d3cd9a164e7f31aa2b4f98c958c15652424c71
m-jaekel <m-jaekel>
2022-12-05 18:50:33 +0100

    Day05: add input data
    Signed-off-by: m-jaekel <m-jaekel>

--- src/day05/Day05_Part1.kt
+++ src/day05/Day05_Part1.kt
@@ -1,11 +1,11 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): String {
+        return input.first()
     }
 
     val testInput = readInput("day05/test_input")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == "CMZ")
 
     val input = readInput("day05/input")
     println(part1(input))
